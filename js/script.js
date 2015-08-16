94862dbd3ce16d52f21fdecaf59d1a8133a2ecc4
Johnny <johnnychan0302@gmail.com>
2015-08-16 18:08:54 +0100

    first attempt to add NYT articles - no luck

--- js/script.js
+++ js/script.js
@@ -12,8 +12,6 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
-
-    // YOUR CODE GOES HERE!
     var streetStr = $('#street').val();
     var cityStr = $('#city').val();
     var address = streetStr + ', ' + cityStr;
@@ -23,6 +21,20 @@ function loadData() {
     var streetViewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + ''; 
 
     $body.append('<img class="bgimg" src="' + streetViewUrl + '">')
+
+    // load New York Times (NYT) articles
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=f062c483a5136eb9a300fcf890a8600f:14:72698594'
+    $.getJASON(nytimesUrl, function(data){
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+        articles = data.response.docs;
+        for (var i=0; i<articles.length; ++i) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">' +
+                '<a href="' + article.web_urls + '">' +
+                article.headline.main + '</a>' +
+                '<p>' + article.snippet + '</p>' + '</li>');
+        };
+    })
 
     return false;
 };
